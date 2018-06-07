7078a24ddbe5eea7ec1bf5d08ff4b4ebe977cd5d
Alexandra Fren <alexandra.fren@gmail.com>
2018-06-07 15:43:31 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(string)
   array = string.split(" ")
-  #array.map do |word|
-  #  array[word].reverse
-  #end
+  array.map do |word|
+    word.reverse
+  end
 #  array.join
   #return array
 end
